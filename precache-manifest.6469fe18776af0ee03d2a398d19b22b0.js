b6e566ba2c9b3f371b938efc1b6b48bc8339895f
Austin Paul <safeat3rd@hotmail.com>
2018-10-12 18:24:14 -0500

    Updates

---
+++ precache-manifest.6469fe18776af0ee03d2a398d19b22b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/csc394-group-2-project/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f47d0184d1510eb436c6",
+    "url": "/csc394-group-2-project/static/js/runtime~main.f47d0184.js"
+  },
+  {
+    "revision": "1ddd4a09393c61ded219",
+    "url": "/csc394-group-2-project/static/js/main.1ddd4a09.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/csc394-group-2-project/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "1ddd4a09393c61ded219",
+    "url": "/csc394-group-2-project/static/css/main.ed2b2af7.chunk.css"
+  },
+  {
+    "revision": "7c9125e41de40d7319eff6c372b5fd54",
+    "url": "/csc394-group-2-project/index.html"
+  }
+];
